fcf1bc732eb26c60b8eaeb4d9d7dbfeda0034948
Isaac Fierro <isaaczid@hotmail.com>
2021-09-09 20:29:56 -0700

    Problema 2 terminado

--- problema1.cpp
+++ problema1.cpp
@@ -3,38 +3,106 @@
 
 using namespace std;
 
-//Declaracion de variables gloables
-int sumaEnteros = 0;
-int nuevoEntero = 0;
+
+//Variables globales
+const int numeroPeliculas = 3;
+int precioPeliculas[numeroPeliculas];
+int total = 0;
+int descuento = 0;
+int totalConDescuento = 0;
+int indexPrecioMayor = 0;
+
 
 //Prototipo de funciones
-int solicitarEnteros();
+int mostrarMenu();
+void solicitarPeliculas();
+void determinarCostos();
+void imprimirCostos();
 
 //Funcion principal
 int main(){
-	while(true){
-		system("cls");
-		nuevoEntero = solicitarEnteros();
-		
-		if(nuevoEntero < 0){
-			break;
-		}
-		
-		sumaEnteros += nuevoEntero;
-		cout<<"El valor actual de la suma es: "<<sumaEnteros<<endl;
-	}
+	while(mostrarMenu() != 2){
+		solicitarPeliculas();
+		determinarCostos();
+		imprimirCostos();
+	}	
 	
-	cout<<"La suma total de los numero enteros es: "<<sumaEnteros<<endl;
+	cout<<"Adios"<<endl;
 	
 	system("pause");
 	return 0;
 }
 
-int solicitarEnteros(){
-	int numero = 0;
+int mostrarMenu(){
+	int opcion = 0;
 	
-	cout<<"Introduzca un numero entero: ";
-	cin>>numero;	
+	cout<<"INTRODUZCA EL NUMERO DE LA ACCION QUE DESEA REALIZAR"<<endl;
+	cout<<"1.- Cobrar peliculas"<<endl;
+	cout<<"2.- Terminar programa"<<endl;
+	cout<<"Introduzca la opcion: ";
+	cin>>opcion;
 	
-	return numero;
+	while(opcion < 1 || opcion > 2){
+		cout<<"La opcion elegida no existe, por favor intentalo de nuevo"<<endl;
+		cin>>opcion;
+	}
+	cout<<""<<endl;
+	
+	return opcion;
 }
+
+void solicitarPeliculas(){
+	for(int i = 0;i < numeroPeliculas;i++){
+		cout<<"Introduzca el precio de la "<<i + 1<<" pelicula: ";
+		cin>>precioPeliculas[i];
+	}
+	cout<<""<<endl;	
+}
+
+void determinarCostos(){
+	int precioMayor = 0;
+	
+	//Suma de los precios
+	for(int i = 0;i < numeroPeliculas;i++){
+		total += precioPeliculas[i];
+	}
+	
+	//Posicion del precio mayor
+	for(int i = 0;i < numeroPeliculas;i++){
+		if(precioPeliculas[i] > precioMayor){
+			precioMayor = precioPeliculas[i];
+			indexPrecioMayor = i;
+		}
+	}
+	
+	//Determinar descuento
+	for(int i = 0;i < numeroPeliculas;i++){
+		if(i == indexPrecioMayor){
+			continue;
+		}else{
+			descuento += precioPeliculas[i]; 
+		}
+	}
+	
+	totalConDescuento = total - descuento;
+}
+
+void imprimirCostos(){
+	cout<<"El valor total de las peliculas es: "<<total<<endl;
+	cout<<"El descuento es de: "<<descuento<<endl;
+	cout<<"El total neto es de: "<<totalConDescuento<<endl;
+	cout<<""<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
