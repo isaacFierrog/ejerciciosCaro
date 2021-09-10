bfca6e79364f27d010c1937a13fb7f5dd5f5357d
Isaac Fierro <isaaczid@hotmail.com>
2021-09-09 20:47:12 -0700

    Problema3 terminado

--- problema1.cpp
+++ problema1.cpp
@@ -5,27 +5,18 @@ using namespace std;
 
 
 //Variables globales
-const int numeroPeliculas = 3;
-int precioPeliculas[numeroPeliculas];
-int total = 0;
-int descuento = 0;
-int totalConDescuento = 0;
-int indexPrecioMayor = 0;
+int limInferior = 0;
+int limSuperior = 0;
 
 
 //Prototipo de funciones
-int mostrarMenu();
-void solicitarPeliculas();
-void determinarCostos();
-void imprimirCostos();
+void solicitarDatos();
+void imprimirSerieNumerica();
 
 //Funcion principal
 int main(){
-	while(mostrarMenu() != 2){
-		solicitarPeliculas();
-		determinarCostos();
-		imprimirCostos();
-	}	
+	solicitarDatos();
+	imprimirSerieNumerica();
 	
 	cout<<"Adios"<<endl;
 	
@@ -33,67 +24,45 @@ int main(){
 	return 0;
 }
 
-int mostrarMenu(){
-	int opcion = 0;
+void solicitarDatos(){
+	cout<<"Introduzca el limite inferior: ";
+	cin>>limInferior;
+	cout<<"Introduzca el limite superior: ";
+	cin>>limSuperior;
 	
-	cout<<"INTRODUZCA EL NUMERO DE LA ACCION QUE DESEA REALIZAR"<<endl;
-	cout<<"1.- Cobrar peliculas"<<endl;
-	cout<<"2.- Terminar programa"<<endl;
-	cout<<"Introduzca la opcion: ";
-	cin>>opcion;
-	
-	while(opcion < 1 || opcion > 2){
-		cout<<"La opcion elegida no existe, por favor intentalo de nuevo"<<endl;
-		cin>>opcion;
+	while(limInferior > limSuperior){
+		cout<<"\nEl limite inferior no puede ser mayor que el limite superior, vuelva a ingresar los valores"<<endl;
+		cout<<"Introduzca el limite inferior: ";
+		cin>>limInferior;
+		cout<<"Introduzca el limite superior: ";
+		cin>>limSuperior;	
 	}
+	
 	cout<<""<<endl;
-	
-	return opcion;
 }
 
-void solicitarPeliculas(){
-	for(int i = 0;i < numeroPeliculas;i++){
-		cout<<"Introduzca el precio de la "<<i + 1<<" pelicula: ";
-		cin>>precioPeliculas[i];
-	}
-	cout<<""<<endl;	
-}
-
-void determinarCostos(){
-	int precioMayor = 0;
+void imprimirSerieNumerica(){
+	string parOImpar = "";
 	
-	//Suma de los precios
-	for(int i = 0;i < numeroPeliculas;i++){
-		total += precioPeliculas[i];
-	}
-	
-	//Posicion del precio mayor
-	for(int i = 0;i < numeroPeliculas;i++){
-		if(precioPeliculas[i] > precioMayor){
-			precioMayor = precioPeliculas[i];
-			indexPrecioMayor = i;
-		}
-	}
-	
-	//Determinar descuento
-	for(int i = 0;i < numeroPeliculas;i++){
-		if(i == indexPrecioMayor){
-			continue;
+	for(int i = limSuperior;i >= limInferior;i--){
+		if(i % 2 == 0){
+			parOImpar = "(par)";
 		}else{
-			descuento += precioPeliculas[i]; 
+			parOImpar = "(impar)";
+		}
+		
+		if(i == 0){
+			parOImpar = "(numero neutro)";
+		}
+		
+		if(i == limInferior){
+			cout<<i<<parOImpar<<endl;
+		}else{
+			cout<<i<<parOImpar<<" - ";	
 		}
 	}
-	
-	totalConDescuento = total - descuento;
-}
-
-void imprimirCostos(){
-	cout<<"El valor total de las peliculas es: "<<total<<endl;
-	cout<<"El descuento es de: "<<descuento<<endl;
-	cout<<"El total neto es de: "<<totalConDescuento<<endl;
 	cout<<""<<endl;
 }
-
 
 
 
